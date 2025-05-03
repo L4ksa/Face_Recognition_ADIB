7848d3e75a3af64532497f7b5caf15a5302ccc9a
L4ksa <155923207+L4ksa@users.noreply.github.com>
2025-05-04 07:44:11 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+# setup.sh
+
+# Update and install necessary libraries
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx
+
+# Install other necessary libraries (you can add more if needed)
+sudo apt-get install -y libglib2.0-0
