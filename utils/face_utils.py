3a8d131d2dd38c3af43e14798a25b90c0c264b95
L4ksa <155923207+L4ksa@users.noreply.github.com>
2025-05-06 23:41:12 +0800

    Update face_utils.py

--- utils/face_utils.py
+++ utils/face_utils.py
@@ -3,20 +3,13 @@ import numpy as np
 from deepface import DeepFace
 
 def align_face(image):
-    """
-    Aligns the face using DeepFace's automatic alignment.
-    Returns the cropped and aligned face image or None if no face is detected.
-    """
     try:
         aligned_face = DeepFace.detectFace(image, detector_backend='opencv', enforce_detection=True)
         return aligned_face
-    except Exception as e:
-        return None  # Return None if no face is detected
+    except Exception:
+        return None
 
 def get_face_embeddings(face_image, model_name="ArcFace"):
-    """
-    Given a cropped face (160x160), return the 128-d face embedding using DeepFace.
-    """
     try:
         embedding = DeepFace.represent(face_image, model_name=model_name, enforce_detection=False)
         if embedding:
